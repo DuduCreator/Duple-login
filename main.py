7aefea2da46153d2aeec50ca6a47c3c30e38e9f8
DuduCreator <117525771+DuduCreator@users.noreply.github.com>
2024-09-02 23:21:55 +0300

    Update main.py
    redesigned how main.py works
    
    added classes

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 import csv
 import re
 
+class Login():
+    def correct(self):
+        with open("login.csv", "r") as file:
+            reader = csv.DictReader(file)
+
+            for row in reader:
+                if row["user"] == self.username and row["pass"] == self.password:
+                    return "Correct"
+                else:
+                    return "Incorrect password"
+
+
 def main():
+    login = Login()
+    
     while True:
         username = input("Username: ")
         password = input("Password: ")
-        if not re.search(r"^\w*$", username) and not re.search(r"^\w*$", password):
-            print("Invalid format. Only letters, numbers and underscores allowed!")
-        else:
-            break
         
-    with open("login.csv", "r") as file:
-        reader = csv.DictReader(file)
-            
-        for row in reader:
-            if row["user"] == username and row["pass"] == password:
-                login(username)
-            else:
-                exit("Incorrect password")
+        login.username = username
+        login.password = password
+        
+        print(login)
+        if login.correct() == "Correct":
+            break
+        else:
+            print(login.correct())
 
-def login(user):
     while True:
-        response = input(f"Good day, {user}! What would you like to do? Log out (/) or add a new password (+)\n")
+        response = input(f"Good day, {login.username}! What would you like to do? Log out (/) or add a new password (+)\n")
         if response == "/":
             exit("\nLogged out successfully. Thanks for using Duple-login for logging in. I hope you have a great day!")
+        elif response == "+":
+            create()
 
-    
+
+def create():
+    ... # another day
+
 
 if __name__ == "__main__":
     main()
